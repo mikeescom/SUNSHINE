6e0867fefaa1c17f0ad57b13f734cf3289e1bc4f
mikeescom <mike_matematicas@ciencias.unam.mx>
2016-04-17 20:11:28 -0400

    Settings change

--- app/src/main/java/com/sunshine/android/sunshine/ForecastFragment.java
+++ app/src/main/java/com/sunshine/android/sunshine/ForecastFragment.java
@@ -122,16 +122,15 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
         super.onActivityCreated(savedInstanceState);
     }
 
+    void onLocationChanged( ) {
+        updateWeather();
+        getLoaderManager().restartLoader(FORECAST_LOADER, null, this);
+    }
+
     private void updateWeather() {
         FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
         String location = Utility.getPreferredLocation(getActivity());
         weatherTask.execute(location);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        updateWeather();
     }
 
     @Override
