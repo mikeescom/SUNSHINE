adafe227910c6db02b153c7912f897b579bb0ba4
Miguel Saavedra Lozano <miguelsaavedralozano@Miguels-MacBook-Pro.local>
2017-10-29 13:25:12 -0700

    Upgrade

--- app/src/main/java/com/sunshine/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/sunshine/android/sunshine/DetailActivity.java
@@ -1,14 +1,7 @@
 package com.sunshine.android.sunshine;
 
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-
 
 public class DetailActivity extends AppCompatActivity {
 
